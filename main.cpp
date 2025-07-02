d0ea0f4308452305034f00ddf8037dda94f518f7
DavidFu1324 <uhuvysuh6736879@outlook.com>
2025-07-02 05:23:03 -0700

    Update: added print line for Hello Git again *2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
 int main() {
-    std::cout << "Hello GitHub from VS Code! yeah~" << std::endl;
+    std::cout << "Hello GitHub from VS Code! yeah~ yes!!" << std::endl;
     return 0;
 }
